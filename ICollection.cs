78ad0459718990c813b4073799cc30c588b48f65
hl2703 <s10186258@connect.np.edu.sg>
2023-02-03 12:51:00 +0800

    Add collection interface

---
+++ ICollection.cs
@@ -0,0 +1,15 @@
+﻿//Implementation of ICollection for Iterator Design Pattern(Hannah)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    public interface ICollection
+    {
+        public abstract IIterator CreateIterator();
+    }
+}
+
