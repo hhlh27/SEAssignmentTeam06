10b9309d91b449ad8d9ab2d41786f5c910f7a6bd
hl2703 <s10186258@connect.np.edu.sg>
2023-02-03 12:53:19 +0800

    Add HotelCollection and HotelIterator classes

---
+++ HotelCollection.cs
@@ -0,0 +1,30 @@
+﻿//Implementation of HotelCollection Class for Iterator Design Pattern(Hannah)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    public class HotelCollection : ICollection
+    {
+        List<Hotel> items = new List<Hotel>();
+        public IIterator CreateIterator()
+        {
+            return new HotelIterator(this);
+        }
+        // Get item count
+        public int Count
+        {
+            get { return items.Count; }
+        }
+        //indexer
+        public object this[int index]
+        {
+            get { return items[index]; }
+            set { items.Add((Hotel)value); }
+        }
+    }
+
+}
---
+++ HotelIterator.cs
@@ -0,0 +1,53 @@
+﻿//Implementation of HotelIterator for Iterator Design Pattern(Hannah)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    public class HotelIterator : IIterator
+    {
+        HotelCollection collection;
+        int current = 0;
+        int step = 1;
+        // Constructor
+        public HotelIterator(HotelCollection collection)
+        {
+            this.collection = collection;
+        }
+        // Gets first item
+        public Hotel First()
+        {
+            current = 0;
+            return collection[current] as Hotel;
+        }
+        // Gets next item
+        public Hotel Next()
+        {
+            current += step;
+            if (!IsDone)
+                return collection[current] as Hotel;
+            else
+                return null;
+        }
+        // Gets or sets stepsize
+        public int Step
+        {
+            get { return step; }
+            set { step = value; }
+        }
+        // Gets current iterator item
+        public Hotel CurrentItem
+        {
+            get { return collection[current] as Hotel; }
+        }
+        // Gets whether iteration is complete
+        public bool IsDone
+        {
+            get { return current >= collection.Count; }
+        }
+    }
+}
+
