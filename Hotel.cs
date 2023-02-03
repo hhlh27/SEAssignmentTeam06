6612bfe14787dc3b2e296ac7818ff3d4f8504c21
hl2703 <s10186258@connect.np.edu.sg>
2023-02-03 12:48:19 +0800

    Add Hotel class and iterator interface

---
+++ Hotel.cs
@@ -0,0 +1,30 @@
+﻿//Implementation of Hotel Class(Hannah)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string HotelName { get; set; }
+        public string Location { get; set; }
+        public string HotelType { get; set; }
+        public bool IsVouchersAllowed { get; set; }
+        public int ReviewScore { get; set; }
+
+        public Hotel(int id, string n, string l, string ht, bool iv, int r)
+        {
+            HotelId = id;
+            HotelName = n;
+            Location = l;
+            HotelType = ht;
+            IsVouchersAllowed = iv;
+            ReviewScore = r;
+        }
+
+    }
+}
---
+++ IIterator.cs
@@ -0,0 +1,19 @@
+﻿//Implementation of IIterator for Iterator Design Pattern(Hannah)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    public interface IIterator
+    {
+
+        Hotel First();
+        Hotel Next();
+        bool IsDone { get; }
+        Hotel CurrentItem { get; }
+
+    }
+}
