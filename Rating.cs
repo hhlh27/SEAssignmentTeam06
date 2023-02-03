e3ed3b210d87a1a8081d981e275069cda64e7fe1
hl2703 <s10186258@connect.np.edu.sg>
2023-02-03 12:49:36 +0800

    Add Rating class

---
+++ Rating.cs
@@ -0,0 +1,19 @@
+﻿//Implementation of Rating Class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    public class Rating
+    {
+        public int StarRating { get; set; }
+        public string Review { get; set; }
+
+        //Rating methods for Observer Pattern(Caleb)
+
+
+    }
+}
