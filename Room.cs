41c42ff550d2047c5a528bf3c58561a568ba5410
lim44104 <s10207455@connect.np.edu.sg>
2023-02-05 12:26:39 +0800

    Add room class

---
+++ Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomType { get; set; }
+        public string BedType { get; set; }
+        public bool IsBreakfastServed { get; set; }
+        public int MaxNumGuests { get; set; }
+        public double Cost { get; set; }
+
+        public Room(int roomId, string roomType, string bedType, bool isBreakfastServed, int maxGuest, double cost)
+        {
+            RoomId = roomId;
+            RoomType = roomType;
+            BedType = bedType;
+            IsBreakfastServed = isBreakfastServed;
+            MaxNumGuests = maxGuest;
+            Cost = cost;
+        }
+    }
+}
