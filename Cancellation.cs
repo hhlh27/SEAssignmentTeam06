ebb3e7d40a7de62fb099de62c1320384e5498608
lim44104 <s10207455@connect.np.edu.sg>
2023-02-05 12:46:32 +0800

    Add cancellation class

---
+++ Cancellation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    class Cancellation
+    {
+        public string CancellationId { get; set; }
+        public DateTime CancellationDate { get; set; }
+        public double AmtRefunded { get; set; }
+
+        public Cancellation(string cancellationId, DateTime cancellationDate, double amtRefund)
+        {
+            CancellationId = cancellationId;
+            CancellationDate = cancellationDate;
+            AmtRefunded = amtRefund;
+        }
+    }
+}
