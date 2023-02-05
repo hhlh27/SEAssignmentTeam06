0623911a0e584b9de8599656ec25f0e572e7e211
lim44104 <s10207455@connect.np.edu.sg>
2023-02-05 12:41:28 +0800

    Add reservation class

---
+++ Reservation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEAssignment
+{
+    class Reservation
+    {
+        public int ReservationId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public string ReservationStatus { get; set; }
+        public bool IsFullyPaid { get; set; }
+        public DateTime DatePaid { get; set; }
+
+        public Reservation(int reservationId, DateTime checkInDate, DateTime checkOutDate, string reservationStatus, bool isFullyPaid, DateTime datePaid)
+        {
+            ReservationId = reservationId;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+            ReservationStatus = reservationStatus;
+            IsFullyPaid = isFullyPaid;
+            DatePaid = datePaid;
+        }
+
+        public bool confirmReservation(bool isFullyPaid)
+        {
+            //implementation
+            return true;
+        }
+
+        public void searchHotel(double budget, string hotelType, double reviewScore, string location, string facilities)
+        {
+            //implementation
+        }
+
+    }
+}
