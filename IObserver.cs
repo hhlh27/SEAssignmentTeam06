a7ee90c7213d4bf02d7a4fe0fdda6a338ceae3f6
CalebChoo1 <S10205375@connect.np.edu.sg>
2023-02-10 15:49:19 +0800

    merge

--- IObserver.cs
+++ IObserver.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace SEAssignment
 {
-    // Define a generic interface for observers (subscribers) specifying how they should be updated.
-    // In traditional observer pattern, the Update method doesn’t specify any parameters and observers keep a reference of the observable (publisher).
-    // However, this approach usually creates a problem like memory leak if the observers and observables are not cleansed properly.
     public interface IObserver
     {
         void Update(Rating rating);
